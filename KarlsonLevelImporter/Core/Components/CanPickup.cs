cfc29befc0d8a7bbbddac73f75b2a00078f7fe32
Jor02 <Jor02games@gmail.com>
2021-08-09 17:45:32 +0200

    Added CanPickup
    Used to fix not being able to grab weapons

---
+++ KarlsonLevelImporter/Core/Components/CanPickup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace KarlsonLevelImporter.Core.Components
+{
+    class CanPickup : ComponentBase
+    {
+        protected override void StartComp()
+        {
+            gameObject.tag = "Gun";
+        }
+    }
+}
